86fe9e1ea93058358646147156dbf7d612a07ab5
supermonkeyz <popbaby@qq.com>
2020-11-26 23:37:01 +0800

    [@vue/compiler-sfc] compileTemplate now requires the uid=197608(Jop) gid=197121 groups=197121 option.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ try {
   `);
 }
 
+import path from 'path';
+import hash from 'hash-sum';
 import SVGO from 'svgo';
 import { createFilter } from '@rollup/pluginutils';
 import { compileTemplate } from '@vue/compiler-sfc';
@@ -23,11 +25,22 @@ async function optimizeSVG(data, path, config) {
   }
 }
 
+function genScopeId(code, filename) {
+  const sourceRoot = process.cwd();
+  const shortFilePath = path
+    .relative(sourceRoot, filename)
+    .replace(/^(\.\.[\/\\])+/, '')
+    .replace(/\\/g, '/');
+  const scopeId = hash(shortFilePath + '\n' + code);
+  return scopeId;
+}
+
 const defaultOptions = {
   include: /\.svg$/,
   exclude: [],
   svgo: {},
   sourceMap: false,
+  target: 'browser',
 };
 
 export default function pluginSvgToVue(options = {}) {
@@ -42,6 +55,8 @@ export default function pluginSvgToVue(options = {}) {
       const compiled = compileTemplate({
         source: optimizedSource.data,
         filename: id,
+        id: genScopeId(optimizedSource.data, id),
+        ssr: options.target === 'node',
       });
       let code = compiled.code;
       const sourceMap = compiled.map;
